e9d1193e7d444b06e6cc7c19df6bb78c96c6b628
YaoZzzzhao <53183448+YaoZzzzhao@users.noreply.github.com>
2019-08-16 11:24:19 -0400

    finalVersion

--- src/test/java/com/ascending/training/repository/HibernateCatTest.java
+++ src/test/java/com/ascending/training/repository/HibernateCatTest.java
@@ -40,12 +40,12 @@ public class HibernateCatTest {
         catDaoImpl.saveCat(a,user);
     }
 
-//    @After
-//    public void cleanUp(){
-//        catDaoImpl.delete(a.getId());
-//        catDaoImpl = null;
-//        assertNull(catDaoImpl);
-//    }
+   @After
+   public void cleanUp(){
+       catDaoImpl.delete(a.getId());
+       catDaoImpl = null;
+       assertNull(catDaoImpl);
+   }
 
 
 
@@ -89,6 +89,4 @@ public class HibernateCatTest {
 
         assertEquals(testId, test.getId());
     }
-
-
 }
